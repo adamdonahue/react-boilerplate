21ef21df4cbfc9ccd4a8b33f1608a4858ca04e4e
Adam Donahue <adam.donahue@gmail.com>
2015-11-20 13:52:44 -0500

    Lint-related fixes.

--- src/js/components/input.js
+++ src/js/components/input.js
@@ -1,32 +1,8 @@
 import React from 'react';
 
-/*
-export default class Input extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  updateValue(e) {
-    this.setState({
-      value: e.target.value
-    });
-  }
-  render() {
-    console.log('rendering');
-    return (
-      <div>
-        <input type="text" value={this.state.value} onChange={this.updateValue.bind(this)} />
-        <h1>{this.state.value}</h1>
-      </div>
-    );
-  }
-}
-*/
-
-
 var Input = React.createClass({
   getInitialState: function () {
-    return {}
+    return {};
   },
   updateValue: function (e) {
     this.setState({value: e.target.value});
