87d401061caf23e39ca99271aaa036494da94f6c
Adam Donahue <adam.donahue@gmail.com>
2015-11-20 13:42:32 -0500

    Lint-related fixes.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-console, no-undef */
+
 var path = require('path');
 
 global.root_require = function root_require(name) {
-    return require(path.join(__dirname, name));
+  return require(path.join(__dirname, name));
 };
 
 var express = require('express');
@@ -9,7 +11,7 @@ var express = require('express');
 var config = root_require('config');
 var router = root_require('router');
 
-app = express();
+var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,5 +20,5 @@ app.use(config.public.route, express.static(config.public.folder));
 app.use('/', router);
 
 app.listen(config.port, function() {
-    console.log("Started HTTP server on port", config.port);
+  console.log('Started HTTP server on port', config.port);
 });
